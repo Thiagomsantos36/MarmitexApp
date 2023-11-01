9f05fceb04c95d2d583cd02f21f6259b4f62040d
Thiagomsantos36 <107224733+Thiagomsantos36@users.noreply.github.com>
2023-11-01 14:22:17 -0300

    MarmitexApp

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './src/LoginScreen'; // Importe a tela de login
+import Sales from './src/vendasScreen'; // Importe a tela de vendas
 
-export default function App() {
+const Stack = createStackNavigator(); // Cria uma instância do Stack Navigator
+
+export default function App() { // Função principal do aplicativo
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer> {/* Contêiner de navegação raiz */}
+      <Stack.Navigator initialRouteName="Login"> 
+        <Stack.Screen name="Login" component={Login} /> 
+        <Stack.Screen name="Vendas" component={Sales} /> 
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//  este código configura a estrutura de navegação do aplicativo usando o React Navigation.
+// Ele cria um Stack Navigator com duas telas, "Login" e "Vendas", e define a tela "Login" como a tela inicial
+// quando o aplicativo é iniciado. Quando o aplicativo é executado, a navegação entre essas telas 
+//será gerenciada automaticamente pelo Stack Navigator, permitindo que o usuário navegue entre as telas de forma eficiente.
